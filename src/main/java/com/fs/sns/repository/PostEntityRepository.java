58e1f7fff2216490da6bcc10c07166ac382b170f
IronKim <tankokim@gmail.com>
2024-03-27 20:07:02 +0900

    피드 목록 기능 테스트 작성하기

--- src/main/java/com/fs/sns/repository/PostEntityRepository.java
+++ src/main/java/com/fs/sns/repository/PostEntityRepository.java
@@ -1,7 +1,12 @@
 package com.fs.sns.repository;
 
 import com.fs.sns.model.Entity.PostEntity;
+import com.fs.sns.model.Entity.UserEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostEntityRepository extends JpaRepository<PostEntity, Integer>{
+
+    Page<PostEntity> findAllByUser(UserEntity entity, Pageable pageable);
 }
--- src/main/java/com/fs/sns/service/PostService.java
+++ src/main/java/com/fs/sns/service/PostService.java
@@ -9,7 +9,10 @@ import com.fs.sns.repository.PostEntityRepository;
 import com.fs.sns.repository.UserEntityRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
 
 @Service
 @RequiredArgsConstructor
@@ -59,5 +62,16 @@ public class PostService {
         }
 
         postEntityRepository.delete(postEntity);
+    }
+
+    public Page<Post> list(Pageable pageable) {
+        return postEntityRepository.findAll(pageable).map(Post::fromEntity);
+    }
+
+    public Page<Post> my(String userName, Pageable pageable) {
+        UserEntity userEntity = userEntityRepository.findByUserName(userName).orElseThrow(() ->
+                new SnsApplicationException(ErrorCode.USER_NOT_FOUND, String.format("%s not founded", userName)));
+
+        return postEntityRepository.findAllByUser(userEntity, pageable).map(Post::fromEntity);
     }
 }
