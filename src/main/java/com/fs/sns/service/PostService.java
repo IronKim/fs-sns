592572bcfe813bc1d48bd11363e91b9d948184fd
IronKim <tankokim@gmail.com>
2024-03-28 20:15:45 +0900

    좋아요 기능 테스트 작성하기

--- src/main/java/com/fs/sns/service/PostService.java
+++ src/main/java/com/fs/sns/service/PostService.java
@@ -74,4 +74,9 @@ public class PostService {
 
         return postEntityRepository.findAllByUser(userEntity, pageable).map(Post::fromEntity);
     }
+
+    @Transactional
+    public void like(Integer postId, String userName) {
+
+    }
 }
