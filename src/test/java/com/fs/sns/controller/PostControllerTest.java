326029e9e01a6018b73ddc2dd019375a2149aa6a
IronKim <tankokim@gmail.com>
2024-03-28 20:32:01 +0900

    좋아요 기능 테스트 수정

--- src/test/java/com/fs/sns/controller/PostControllerTest.java
+++ src/test/java/com/fs/sns/controller/PostControllerTest.java
@@ -223,18 +223,18 @@ public class PostControllerTest {
     @Test
     @WithAnonymousUser
     void 좋아요버튼클릭시_로그인하지_않은경우() throws Exception {
-        mockMvc.perform(get("/api/v1/posts/1/likes")
+        mockMvc.perform(post("/api/v1/posts/1/likes")
                         .contentType(MediaType.APPLICATION_JSON)
                 ).andDo(print())
                 .andExpect(status().isUnauthorized());
     }
 
     @Test
-    @WithAnonymousUser
+    @WithMockUser
     void 좋아요버튼클릭시_게시물이_없는경우() throws Exception {
         doThrow(new SnsApplicationException(ErrorCode.POST_NOT_FOUND)).when(postService).like(any(), any());
 
-        mockMvc.perform(get("/api/v1/posts/1/likes")
+        mockMvc.perform(post("/api/v1/posts/1/likes")
                         .contentType(MediaType.APPLICATION_JSON)
                 ).andDo(print())
                 .andExpect(status().isNotFound());
