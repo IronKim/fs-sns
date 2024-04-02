7db89806d79a19437d74eaa35e3ef25e375c320b
IronKim <tankokim@gmail.com>
2024-04-02 14:48:56 +0900

    see를 통한 알람 개선하기

--- src/main/java/com/fs/sns/controller/UserController.java
+++ src/main/java/com/fs/sns/controller/UserController.java
@@ -8,6 +8,7 @@ import com.fs.sns.controller.request.UserJoinRequest;
 import com.fs.sns.exception.ErrorCode;
 import com.fs.sns.exception.SnsApplicationException;
 import com.fs.sns.model.User;
+import com.fs.sns.service.AlarmService;
 import com.fs.sns.service.UserService;
 import com.fs.sns.util.ClassUtils;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +16,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -22,6 +24,7 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     private final UserService userService;
+    private final AlarmService alarmService;
 
     @PostMapping("/join")
     public Response<UserJoinResponse> join(@RequestBody UserJoinRequest request) {
@@ -40,6 +43,13 @@ public class UserController {
         User user = ClassUtils.getSafeCastInstance(authentication.getPrincipal(), User.class).orElseThrow(() ->
                 new SnsApplicationException(ErrorCode.INTERNAL_SERVER_ERROR, "Casting to User class failed"));
         return Response.success(userService.alarmList(user.getId(), pageable).map(AlarmResponse::fromAlarm));
+    }
+
+    @GetMapping("/alarm/subscribe")
+    public SseEmitter subscribe(Authentication authentication) {
+        User user = ClassUtils.getSafeCastInstance(authentication.getPrincipal(), User.class).orElseThrow(() ->
+                new SnsApplicationException(ErrorCode.INTERNAL_SERVER_ERROR, "Casting to User class failed"));
+        return alarmService.connectAlarm(user.getId());
     }
 
 
